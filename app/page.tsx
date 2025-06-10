77b8672940bd4f66c18a5834fb1b348213206ad6
josue david kennedy <104701497+programfive@users.noreply.github.com>
2025-06-10 00:12:50 -0400

    configuration eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
+import { DeployButton } from '@/components/deploy-button';
+import { EnvVarWarning } from '@/components/env-var-warning';
+import { AuthButton } from '@/components/auth-button';
+import { ThemeSwitcher } from '@/components/theme-switcher';
+import { hasEnvVars } from '@/lib/utils';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,7 +12,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={'/'}>Next.js Supabase Starter</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
               </div>
@@ -24,16 +21,12 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
+          estamos funcionando normalmente 💐💐💐
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Powered by{" "}
+            Powered by{' '}
             <a
               href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
               target="_blank"
